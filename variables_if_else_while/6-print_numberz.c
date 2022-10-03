9e840351e72114b9b842c1c5cfffaecf64fad906
annlaure02 <alaure.g02@gmail.com>
2022-10-03 11:30:47 -0700

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.int n;
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = '0';
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
