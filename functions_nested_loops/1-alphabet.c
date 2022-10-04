86c326b3f115b645f66101c6126f0b6aaf3e2992
annlaure02 <alaure.g02@gmail.com>
2022-10-04 10:49:54 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - fonction print_alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	print_alphabet();
+	{
+		char az;
+
+		for (az = 'a' ; az <= 'z' ; az++)
+		{
+			_putchar(az);
+		}
+		_putchar('\n');
+	}
+}
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - fonction print_alphabet
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	 print_alphabet();
+	 return (0);
+}
