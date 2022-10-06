687f107a77443f4f3532741face09bce71b20c85
annlaure02 <alaure.g02@gmail.com>
2022-10-06 08:18:39 -0700

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,21 @@
 
 void print_to_98(int n)
 {
-	while (n >= 98)
+	if (n > 98)
 	{
+		while (n >= 98)
+		{
 		printf("%d, ", n);
 		n--;
+		}
+		printf("\n");
 	}
-	printf("\n");
-
-	while (n <= 98)
-	{
+	else
+		while (n <= 98)
+		{
 		printf("%d, ", n);
 		n++;
-	}
+		}
 	printf("\n");
+
 }
