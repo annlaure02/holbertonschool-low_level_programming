ff55170731e75a0e1cbd779bfa4caf0d0a8ab5d1
annlaure02 <alaure.g02@gmail.com>
2022-10-07 13:41:43 -0700

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - check the code.
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int l, c, r;
+
+	for (l = 0; l < 10; l++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			r = l * c;
+			if (r <= 9)
+			{
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+			if (l != 9)
+			{
+				_putchar(',');
+				if (l * (c+1) <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
