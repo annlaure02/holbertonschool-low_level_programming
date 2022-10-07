429744afcdf47c511d18ed5126c3a81e7fba5b42
annlaure02 <alaure.g02@gmail.com>
2022-10-07 14:27:17 -0700

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int l, c, r;
 
-	for (l = 0; l < 10; l++)
+	for (l = 0; l <= 9; l++)
 	{
-		for (c = 0; c < 10; c++)
+		for (c = 0; c <= 9; c++)
 		{
 			r = l * c;
 			if (r <= 9)
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(r / 10 + '0');
 				_putchar(r % 10 + '0');
 			}
-			if (l != 9)
+			if (c != 9)
 			{
 				_putchar(',');
 				if (l * (c+1) <= 9)
