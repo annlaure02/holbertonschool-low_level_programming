f9eb1f5e7f8018f309c2e4bd35d7c1d3637145f3
annlaure02 <alaure.g02@gmail.com>
2022-10-07 07:10:12 -0700

    Task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - The “Fizz-Buzz test” to help filter out the 99.5% of programming
+ * job candidates who can’t seem to program
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}
