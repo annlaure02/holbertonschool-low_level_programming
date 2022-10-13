4b7fd6627461228fa604e3e0b66023dba9a37665
annlaure02 <alaure.g02@gmail.com>
2022-10-13 11:39:16 -0700

    Task 1 projet 3

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copie memory area
+ *
+ * @dest: parameter of the function
+ * @src: parameter of the function
+ * @n: parameter of the function
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
