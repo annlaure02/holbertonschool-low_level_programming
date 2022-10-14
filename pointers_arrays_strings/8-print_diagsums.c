4d14ec2581766288dd9e928ab3cba07f437e7924
annlaure02 <alaure.g02@gmail.com>
2022-10-14 11:41:28 -0700

    Task 7 projet 3

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -20,7 +20,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		diag1 = diag1 + (a[(size + 1) * i]);
-		diag2 = diag2 + (a[(size - 1) * (i +1)]);
+		diag2 = diag2 + (a[(size - 1) * (i + 1)]);
 	}
 	printf("%d, %d\n", diag1, diag2);
 }
