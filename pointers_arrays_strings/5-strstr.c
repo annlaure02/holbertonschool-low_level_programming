7b3ac576dc809b9b6bab7d39128af167c75b688d
annlaure02 <alaure.g02@gmail.com>
2022-10-14 09:51:06 -0700

    Task 5 projet 3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ *
+ * @haystack: parameter of the function
+ * @needle: parameter of the function
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
+	}
+	return (0);
+}
