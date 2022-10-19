a18edb9a8a6ec9c773f7899688403e88d74987f0
annlaure02 <alaure.g02@gmail.com>
2022-10-19 00:28:45 -0700

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,15 +14,17 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-	int r = a * b;
+	int a, b, r;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	r = a * b;
+
 	printf("%d\n", r);
 
 	return (0);
