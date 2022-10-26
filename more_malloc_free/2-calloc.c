62231395241837b721ae2873fb684e1b2eb53543
annlaure02 <alaure.g02@gmail.com>
2022-10-26 06:42:56 -0700

    Task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array, using malloc 
+ * _calloc - allocates memory for an array, using malloc
  *
  * @nmemb: number of arguments
  * @size: number of arguments
