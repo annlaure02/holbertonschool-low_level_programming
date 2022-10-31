4f81eaec721ac9a2f2745512551c7463e8ceee07
annlaure02 <alaure.g02@gmail.com>
2022-10-31 03:23:08 -0700

    Task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Information about a poppy
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
