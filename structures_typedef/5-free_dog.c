29c70150c1070bc2defe9c21b0f63ef9b2188b6c
annlaure02 <alaure.g02@gmail.com>
2022-10-31 09:46:44 -0700

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Free dogs
+ *
+ * @d: function's parameter
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
