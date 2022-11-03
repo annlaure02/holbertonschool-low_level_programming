a7d48622f0a5dfdb54b197e6f989380ff49c86b1
annlaure02 <alaure.g02@gmail.com>
2022-11-03 12:40:36 -0700

    Task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - sum of all its parameters
  *
- * @n: number of arguments 
+ * @n: number of arguments
  *
  * Return: sum
  */
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, unsigned int);
-	
+
 	va_end(ap);
 
 	return (sum);
