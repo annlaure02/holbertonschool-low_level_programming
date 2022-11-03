c87abde5f714e351f93404faf7f9607ef31f44e1
annlaure02 <alaure.g02@gmail.com>
2022-11-03 04:56:18 -0700

    Task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[2][1] != '\0')
+	if (argv[2][1] != '\0'|| get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
