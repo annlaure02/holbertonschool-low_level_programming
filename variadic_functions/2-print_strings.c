d226ef985c80f3e89acf688a1043eb305b43b7c5
annlaure02 <alaure.g02@gmail.com>
2022-11-04 04:24:29 -0700

    Task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		c = va_arg(ap, char *);
 		if (c == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", c);
 		
