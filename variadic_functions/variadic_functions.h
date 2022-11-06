52d27af34ddffefe8b035797f8971e315d0cada4
annlaure02 <alaure.g02@gmail.com>
2022-11-06 03:56:17 -0800

    header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,19 @@
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct form -  list of types of arguments
+ * @d: char
+ * @a: function's pointer
+ * Description: Longer description
+ */
+typedef struct _print
+{
+	char *b;
+	void (*a)(va_list ap);
+} _print;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
