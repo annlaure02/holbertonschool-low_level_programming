04298e10e3b3ab09b6e3630db6644abac59b1026
annlaure02 <alaure.g02@gmail.com>
2022-11-06 03:55:32 -0800

    Task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+
+/**
+ * _char - print a character
+ * @ap: variable of va list
+ * Return: void
+ */
+void _char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * _int - print a integer
+ * @ap: variable of va list
+ * Return: void
+ */
+void _int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * _float - print a float
+ * @ap: variable of va list
+ * Return: void
+ */
+void _float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * _string - print a string
+ * @ap: variable of va list
+ * Return: void
+ */
+void _string(va_list ap)
+{
+	if (va_arg(ap, char *) == NULL)
+		printf("(nil");
+
+	printf("%s", va_arg(ap, char *));
+}
+
+/**
+ * print_all - print anything
+ *
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	_print prt[] = {
+		{"c", _char},
+		{"i", _int},
+		{"f", _float},
+		{"s", _string},
+		{NULL, NULL}
+	};
+	int x = 0, y = 0;
+	char *separator = ", ";
+	va_list ap;
+
+	va_start(ap, format);
+	while (format && format[x] != '\0')
+	{
+		while (prt[y].a)
+		{
+			if (*(prt[y].b) == format[x])
+			{
+				prt[y].a(ap);
+				printf("%s", separator);
+			}
+			y++;
+		}
+		x++;
+	}
+	printf("\n");
+	va_end(ap);
+}
