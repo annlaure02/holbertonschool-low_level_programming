aa1c48fe1a79867c285903e79b9427b93992b36a
annlaure02 <alaure.g02@gmail.com>
2022-11-15 05:18:20 -0800

    Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,8 +18,10 @@ list_t *add_node(list_t **head, const char *str)
 	dup = strdup(str);
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
+	{
 		free(newNode);
 		return (NULL);
+	}
 	if (dup == NULL)
 	{
 		free(newNode);
