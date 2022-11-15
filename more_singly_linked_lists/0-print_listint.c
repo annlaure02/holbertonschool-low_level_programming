51c8aa658939903126f508e913480ca9cb64084c
annlaure02 <alaure.g02@gmail.com>
2022-11-15 11:03:03 -0800

    Task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list - Print all the elements of a list_t list
+ * print_listint - Print all the elements of a list_t list
  *
  * @h: pointer to head
  *
@@ -14,8 +14,6 @@ size_t print_listint(const listint_t *h)
 
 	for (i = 0; h != NULL; i++)
 	{
-	 	/*if (h->n == NULL)
-			printf("[0] (nil)\n");*/
 		if (h->n != '\0')
 			printf("%d\n", h->n);
 		h = h->next;
