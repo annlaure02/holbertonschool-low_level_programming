ee31b98a3c8cd16811da780d6c0ef85acdf964f8
annlaure02 <alaure.g02@gmail.com>
2022-11-16 01:27:23 -0800

    Task 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ *
+ * @head: pointer of a pointer
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
