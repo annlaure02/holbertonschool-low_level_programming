a73ebd9b8c204151c3285878c3900cc6f861490a
annlaure02 <alaure.g02@gmail.com>
2022-11-16 01:37:14 -0800

    Task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+		free(*head);
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	head = NULL;
+
 }
