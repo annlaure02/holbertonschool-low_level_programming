40a665e630b59a2b52367a11af5564d9ed215eb2
annlaure02 <alaure.g02@gmail.com>
2022-11-17 08:18:53 -0800

    Task 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *delNode;
 	unsigned int i;
-	
+
 	temp = *head;
 	if (temp == NULL)
 		return (-1);
@@ -33,7 +33,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	delNode = temp->next;
 	temp->next = delNode->next;
 	free(delNode);
-	free(temp);
 
 	return (1);
 }
