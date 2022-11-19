0db0f6f0b5a42a38e16ce8660d8b39c01e03fdf5
annlaure02 <alaure.g02@gmail.com>
2022-11-19 04:01:34 -0800

    Task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,20 +13,20 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int mult = 1;
-	unsigned int result;
+	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
 	while (b[i])
-	{ 
+	{
 		i++;
 	}
 	for (i -= 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		
+
 		result += (b[i] - '0') * mult;
 		mult *= 2;
 	}
