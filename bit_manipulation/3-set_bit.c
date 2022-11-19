81cf1d517defc977762de0825a2d36a0d3e185d7
annlaure02 <alaure.g02@gmail.com>
2022-11-19 05:58:41 -0800

    Task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - prints the binary representation of a number
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n |= (1UL << index);
+	return (1);
+}
