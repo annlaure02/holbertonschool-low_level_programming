8330db584a68c023407c32401c431ba3142734c5
annlaure02 <alaure.g02@gmail.com>
2022-11-21 04:43:16 -0800

    Task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print it to the POSIX standard output
+ *
+ * @filename: filename
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	char *buf;
+
+	fd = open(filename, O_RDONLY);
+	buf = malloc(sizeof(char) *letters);
+
+	if (!filename || !letters)
+		return (0);
+	read(fd, buf, letters);
+
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	write(1, buf, letters);
+	return (letters);
+}
