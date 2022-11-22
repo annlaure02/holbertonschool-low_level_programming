99ad5a2053ee04fa937940156cf074b8ade06787
annlaure02 <alaure.g02@gmail.com>
2022-11-22 02:18:15 -0800

    Task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	newNode->n = n;
-	newNode->next = *head;
 	newNode->prev = NULL;
+	newNode->next = *head;
+	if ((*head) != NULL)
+		(*head)->prev = newNode;
+
 	*head = newNode;
 
 	return (newNode);
