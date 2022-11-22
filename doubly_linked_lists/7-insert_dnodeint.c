9fc201bc908de77e759a9d646f43af7bbe79482c
annlaure02 <alaure.g02@gmail.com>
2022-11-22 06:04:23 -0800

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		temp = temp->next;
 	}
-	if (temp == NULL)
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 	if (temp != NULL)
 	{
