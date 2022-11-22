ec6e85f39075f9164aaab4a6990b4a589bbb0a71
annlaure02 <alaure.g02@gmail.com>
2022-11-22 01:38:58 -0800

    Task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,10 +11,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
+
 	for (i = 0; h != NULL; i++)
 	{
-		if (h->n != '\0')
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
