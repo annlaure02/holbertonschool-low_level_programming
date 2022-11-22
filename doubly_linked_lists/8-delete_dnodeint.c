3111e4b2b4e9badc5db88ff271e472cb569bcf06
annlaure02 <alaure.g02@gmail.com>
2022-11-22 08:40:28 -0800

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		(*head)->prev = NULL;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(delNode);
 		return (1);
 	}
