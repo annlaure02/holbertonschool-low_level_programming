4e6a6db0a06701bb6041304d20cbc2c42ed6d6fc
annlaure02 <alaure.g02@gmail.com>
2022-11-25 09:35:07 -0800

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	new->key = strdup(key);
-	if (new->key = strdup(key))
+	if (new->key == NULL)
 		return (0);
 	new->value = dupValue;
 	new->next = ht->array[index];
