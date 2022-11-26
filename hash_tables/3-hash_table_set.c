a6a615f8f2081e6bf16b20c035820a53d07a7065
annlaure02 <alaure.g02@gmail.com>
2022-11-26 11:31:09 -0800

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,12 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	dupValue = strdup(value);
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(new);
-		return (0);
-	}
+
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -35,9 +30,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
+	new = malloc(sizeof(hash_node_t));
 	new->key = strdup(key);
 	if (new->key == NULL)
+	{
+		free(new);
 		return (0);
+	}
 	new->value = dupValue;
 	new->next = ht->array[index];
 	ht->array[index] = new;
