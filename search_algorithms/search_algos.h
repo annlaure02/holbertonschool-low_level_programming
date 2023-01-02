782be1b40275d552ed55c317926225174de0769a
annlaure02 <alaure.g02@gmail.com>
2023-01-02 10:57:20 -0800

    add header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __H_
+#define __H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
